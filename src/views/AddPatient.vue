9becc8ac3d620959da394b93b3aeafaa89ebfa35
Bright-Phiri <bphiri.aki@gmail.com>
2021-12-25 11:47:21 +0200

    fixed selected default values in patient form

--- src/views/AddPatient.vue
+++ src/views/AddPatient.vue
@@ -1,169 +1,170 @@
 <template>
-  <NavBar></NavBar>
-  <AppView>
-    <div class="col-3">
-      <Profile></Profile>
-    </div>
-    <div class="col-9">
-      <div class="card card-add mt-4">
-        <div class="mt-4">
-          <h4>Patient Details</h4>
-          <hr />
+    <NavBar></NavBar>
+    <AppView>
+        <div class="col-3">
+            <Profile></Profile>
         </div>
-        <div class="card-body">
-          <form v-on:submit.prevent="addPatient">
-            <div class="form-group row">
-              <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-6 p-2">
-                <label for="firstname">First Name</label>
-                <input
-                  type="text"
-                  id="firstname"
-                  name="firstname"
-                  class="form-control"
-                  placeholder="First Name"
-                  v-model.trim="patient.first_name"
-                />
-              </div>
-              <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-6 p-2">
-                <label for="lastname">Last Name</label>
-                <input
-                  type="text"
-                  id="lastname"
-                  name="lastname"
-                  class="form-control"
-                  placeholder="Last Name"
-                  v-model.trim="patient.last_name"
-                />
-              </div>
-            </div>
-            <div class="form-group row">
-              <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-6 p-2">
-                <label for="gender">Gender</label>
-                <select
-                  class="form-control"
-                  id="gender"
-                  name="gender"
-                  v-model.trim="patient.gender"
-                >
-                  <option value>Select</option>
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                </select>
-              </div>
-              <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-6 p-2">
-                <label for="birthdate">Date Of Birth</label>
-                <input
-                  type="date"
-                  class="form-control"
-                  id="birthdate"
-                  name="dob"
-                  v-model.trim="patient.dob"
-                />
-              </div>
-            </div>
-            <div class="form-group row">
-              <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-6 p-2">
-                <label for="district">District</label>
+        <div class="col-9">
+            <div class="card card-add mt-4">
+                <div class="mt-4">
+                    <h4>Patient Details</h4>
+                    <hr />
+                </div>
+                <div class="card-body">
+                    <form v-on:submit.prevent="addPatient">
+                        <div class="form-group row">
+                            <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-6 p-2">
+                                <label for="firstname">First Name</label>
+                                <input
+                                    type="text"
+                                    id="firstname"
+                                    name="firstname"
+                                    class="form-control"
+                                    placeholder="First Name"
+                                    v-model.trim="patient.first_name"
+                                />
+                            </div>
+                            <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-6 p-2">
+                                <label for="lastname">Last Name</label>
+                                <input
+                                    type="text"
+                                    id="lastname"
+                                    name="lastname"
+                                    class="form-control"
+                                    placeholder="Last Name"
+                                    v-model.trim="patient.last_name"
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-6 p-2">
+                                <label for="gender">Gender</label>
+                                <select
+                                    class="form-control"
+                                    id="gender"
+                                    name="gender"
+                                    v-model.trim="patient.gender"
+                                >
+                                    <option disabled value>Select</option>
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Female</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-6 p-2">
+                                <label for="birthdate">Date Of Birth</label>
+                                <input
+                                    type="date"
+                                    class="form-control"
+                                    id="birthdate"
+                                    name="dob"
+                                    v-model.trim="patient.dob"
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-6 p-2">
+                                <label for="district">District</label>
 
-                <input
-                  type="text"
-                  id="district"
-                  name="district"
-                  class="form-control"
-                  placeholder="District"
-                  v-model.trim="patient.district"
-                />
-              </div>
-              <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-6 p-2">
-                <label for="village">Village</label>
-                <input
-                  type="text"
-                  id="village"
-                  name="village"
-                  class="form-control"
-                  placeholder="Home Village"
-                  v-model.trim="patient.village"
-                />
-              </div>
+                                <input
+                                    type="text"
+                                    id="district"
+                                    name="district"
+                                    class="form-control"
+                                    placeholder="District"
+                                    v-model.trim="patient.district"
+                                />
+                            </div>
+                            <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-6 p-2">
+                                <label for="village">Village</label>
+                                <input
+                                    type="text"
+                                    id="village"
+                                    name="village"
+                                    class="form-control"
+                                    placeholder="Home Village"
+                                    v-model.trim="patient.village"
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-12 p-2">
+                                <label for="district">Occupation</label>
+                                <select
+                                    class="form-control"
+                                    id="occupation"
+                                    name="occupation"
+                                    v-model="patient.occupation"
+                                >
+                                    <option disabled value="">Select</option>
+                                    <option value="Accountant">Accountant</option>
+                                    <option value="Software Developer">Software Developer</option>
+                                    <option value="Business Owner">Business Owner</option>
+                                    <option value="Mechanic">Mechanic</option>
+                                    <option value="Driver">Driver</option>
+                                    <option value="Welder">Welder</option>
+                                    <option value="Construction worker">Construction worker</option>
+                                    <option value="Painter">Painter</option>
+                                    <option value="Radiologist">Radiologist</option>
+                                    <option value="Student">Student</option>
+                                    <option value="Farmer">Farmer</option>
+                                    <option value="Coal miner">Coal miner</option>
+                                    <option value="Coal miner">Other</option>
+                                </select>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn add mt-2">Add Patient</button>
+                        &nbsp;
+                        <button
+                            type="reset"
+                            class="btn cancel mt-2"
+                        >Cancel</button>
+                    </form>
+                </div>
             </div>
-            <div class="form-group row">
-              <div class="form-group col-xs-5 col-lg-5 col-md-5 col-sm-12 p-2">
-                <label for="district">Occupation</label>
-                <select
-                  class="form-control"
-                  id="occupation"
-                  name="occupation"
-                  v-model="patient.occupation"
-                >
-                  <option value>Select</option>
-                  <option value="Accountant">Accountant</option>
-                  <option value="Software Developer">Software Developer</option>
-                  <option value="Business Owner">Business Owner</option>
-                  <option value="Mechanic">Mechanic</option>
-                  <option value="Driver">Driver</option>
-                  <option value="Welder">Welder</option>
-                  <option value="Construction worker">
-                    Construction worker
-                  </option>
-                  <option value="Painter">Painter</option>
-                  <option value="Radiologist">Radiologist</option>
-                  <option value="Student">Student</option>
-                  <option value="Farmer">Farmer</option>
-                  <option value="Coal miner">Coal miner</option>
-                  <option value="Coal miner">Other</option>
-                </select>
-              </div>
-            </div>
-            <button type="submit" class="btn add mt-2">Add Patient</button>
-            &nbsp;
-            <button type="reset" class="btn cancel mt-2">Cancel</button>
-          </form>
         </div>
-      </div>
-    </div>
-  </AppView>
+    </AppView>
 </template>
 
 <style>
 .cancel {
-  background-color: #7c819c;
-  color: #fff;
-  border-radius: 17px;
+    background-color: #7c819c;
+    color: #fff;
+    border-radius: 17px;
 }
 .cancel:hover {
-  background-color: #7c819c;
-  color: #fff;
-  border-radius: 17px;
+    background-color: #7c819c;
+    color: #fff;
+    border-radius: 17px;
 }
 .add {
-  background-color: #14a800;
-  color: #fff;
-  border-radius: 17px;
+    background-color: #14a800;
+    color: #fff;
+    border-radius: 17px;
 }
 .add:hover {
-  background-color: #3c8224;
-  color: #fff;
-  border-radius: 17px;
+    background-color: #3c8224;
+    color: #fff;
+    border-radius: 17px;
 }
 h4 {
-  margin-left: 10px;
-  color: #14a800;
+    margin-left: 10px;
+    color: #14a800;
 }
 
 hr {
-  margin-left: 10px;
-  margin-right: 20px;
+    margin-left: 10px;
+    margin-right: 20px;
 }
 .card-footer {
-  background-color: #e4ebe4;
+    background-color: #e4ebe4;
 }
 
 .card-header {
-  background-color: #fff;
+    background-color: #fff;
 }
 .card-add {
-  background-color: #fff;
-  border-radius: 10px;
+    background-color: #fff;
+    border-radius: 10px;
 }
 </style>
 <script>
@@ -174,75 +175,75 @@ import NavBar from "@/components/NavBar.vue";
 import AppView from "@/components/AppView.vue";
 
 export default {
-  name: "AddPatient",
-  components: {
-    Profile,
-    NavBar,
-    AppView,
-  },
-  data() {
-    return {
-      patient: {
-        first_name: null,
-        last_name: null,
-        gender: null,
-        dob: null,
-        village: null,
-        district: null,
-        occupation: null,
-      },
-      alert: "",
-      image: "",
-      username: "",
-      email: ""
-    };
-  },
-  methods: {
-    addPatient(event) {
-      if (
-        !this.patient.first_name ||
-        !this.patient.last_name ||
-        !this.patient.gender ||
-        !this.patient.dob ||
-        !this.patient.village ||
-        !this.patient.district ||
-        !this.patient.occupation
-      ) {
-        this.$swal(
-          "Fields validation",
-          "Please enter in all fields",
-          "warning"
-        );
-      } else {
-        let patientPayload = {
-          first_name: this.patient.first_name,
-          last_name: this.patient.last_name,
-          gender: this.patient.gender,
-          dob: this.patient.dob,
-          village: this.patient.village,
-          district: this.patient.district,
-          occupation: this.patient.occupation,
-        };
-        let pHRsAPIEndpoint = `${sessionStorage.getItem("BASE_URL")}/patients`;
-        axios
-          .post(pHRsAPIEndpoint, patientPayload, {
-            headers: {
-              Authorization: `Bearer ${sessionStorage.getItem(
-                "Authorization"
-              )}`,
-            },
-          })
-          .then((response) => {
-            this.alert = response.data.message;
-            this.$swal("Information", this.alert, "success").then(() => {
-              event.target.reset();
-            });
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
+    name: "AddPatient",
+    components: {
+        Profile,
+        NavBar,
+        AppView,
     },
-  },
+    data() {
+        return {
+            patient: {
+                first_name: null,
+                last_name: null,
+                gender: "",
+                dob: null,
+                village: null,
+                district: null,
+                occupation: "",
+            },
+            alert: "",
+            image: "",
+            username: "",
+            email: ""
+        };
+    },
+    methods: {
+        addPatient(event) {
+            if (
+                !this.patient.first_name ||
+                !this.patient.last_name ||
+                !this.patient.gender ||
+                !this.patient.dob ||
+                !this.patient.village ||
+                !this.patient.district ||
+                !this.patient.occupation
+            ) {
+                this.$swal(
+                    "Fields validation",
+                    "Please enter in all fields",
+                    "warning"
+                );
+            } else {
+                let patientPayload = {
+                    first_name: this.patient.first_name,
+                    last_name: this.patient.last_name,
+                    gender: this.patient.gender,
+                    dob: this.patient.dob,
+                    village: this.patient.village,
+                    district: this.patient.district,
+                    occupation: this.patient.occupation,
+                };
+                let pHRsAPIEndpoint = `${sessionStorage.getItem("BASE_URL")}/patients`;
+                axios
+                    .post(pHRsAPIEndpoint, patientPayload, {
+                        headers: {
+                            Authorization: `Bearer ${sessionStorage.getItem(
+                                "Authorization"
+                            )}`,
+                        },
+                    })
+                    .then((response) => {
+                        this.alert = response.data.message;
+                        this.$swal("Information", this.alert, "success").then(() => {
+                            event.target.reset();
+                        });
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
+        },
+    },
 };
 </script>
