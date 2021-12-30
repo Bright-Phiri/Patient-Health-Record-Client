af3afbf9ac93e9624364cd7cd837e996bb278f24
Bright-Phiri <bphiri.aki@gmail.com>
2021-12-30 21:05:04 +0200

    cleaning ui

--- src/components/AppView.vue
+++ src/components/AppView.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container-fluid">
         <div class="row">
-          <div class="col-3">
+          <div class="col-lg-3 col-xl-3 col-md-3 col-sm-5">
              <slot name="profile"></slot>
           </div>
-          <div class="col-9">
+          <div class="col-lg-9 col-xl-9 col-md-9 col-sm-12">
             <slot name="page-content"></slot>
           </div>
         </div>
